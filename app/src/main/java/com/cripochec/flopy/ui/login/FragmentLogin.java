de611b666964f0f0d3b703336dbaafdd73d462a6
Cripochec <124876135+Cripochec@users.noreply.github.com>
2024-03-06 05:52:08 +0500

    update login code

--- app/src/main/java/com/cripochec/flopy/ui/login/FragmentLogin.java
+++ app/src/main/java/com/cripochec/flopy/ui/login/FragmentLogin.java
@@ -1,17 +1,18 @@
 package com.cripochec.flopy.ui.login;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
 import com.westernyey.Flopy.R;
 
 
-public class InputSelection extends Fragment {
+public class FragmentLogin extends Fragment {
 
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_input_selection, container, false);
-        }
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
 }
--- app/src/main/java/com/cripochec/flopy/ui/login/fragment_login.java
+++
@@ -1,8 +0,0 @@
-package com.cripochec.flopy.ui.login;
-
-import androidx.fragment.app.Fragment;
-
-public class fragment_login extends Fragment {
-
-
-}
