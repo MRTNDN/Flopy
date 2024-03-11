767827b4ab042a7117f5693d267be1a1fa9c09f9
Andrey <100sss100sss@gmail.com>
2024-03-11 20:33:12 +0500

    реализованы match. Сделан лист

--- app/src/main/java/com/westernyey/Flopy/ui/match/Matches.java
+++ app/src/main/java/com/westernyey/Flopy/ui/match/Matches.java
@@ -1,19 +1,32 @@
 package com.westernyey.Flopy.ui.match;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import androidx.fragment.app.Fragment;
 
 import com.westernyey.Flopy.R;
 
+import java.util.ArrayList;
 
 public class Matches extends Fragment {
+    private ArrayList<String> data;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_matches, container, false);
+        View view = inflater.inflate(R.layout.fragment_matches, container, false);
+
+        data = new ArrayList<>();
+        data.add("данные");
+        data.add("match");
+
+        ListView listView = view.findViewById(R.id.listView);
+        MyAdapter adapter = new MyAdapter(getActivity(), data);
+        listView.setAdapter(adapter);
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/westernyey/Flopy/ui/match/MyAdapter.java
@@ -0,0 +1,48 @@
+package com.westernyey.Flopy.ui.match;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.westernyey.Flopy.R;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends ArrayAdapter<String> {
+    private Context context;
+    private ArrayList<String> data;
+
+    public MyAdapter(Context context, ArrayList<String> data) {
+        super(context, 0, data);
+        this.context = context;
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.textView = convertView.findViewById(R.id.textView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        String item = getItem(position);
+        viewHolder.textView.setText(item);
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        TextView textView;
+    }
+}
