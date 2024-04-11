4b9c9c49858d4058dd0cdb4bb6b0c5e35d2303f1
Cripochec <124876135+Cripochec@users.noreply.github.com>
2024-04-11 14:44:15 +0500

    fix register code v.3

--- app/src/main/java/com/westernyey/Flopy/ui/register/FragmentRegister.java
+++ app/src/main/java/com/westernyey/Flopy/ui/register/FragmentRegister.java
@@ -40,10 +40,10 @@ public class FragmentRegister extends Fragment {
                 if (pas1.getText().toString().length() >= 8 && pas2.getText().toString().length() >= 8) {
                     // Проверка на совпадение паролей
                     if (pas1.getText().toString().equals(pas2.getText().toString())) {
-                        new RegisterRequestUtils(this, "register_data", "POST", "{\"email\": \"" + new_email + "\", \"password\": \"" + pas1 + "\"}").execute(new_email.getText().toString(), pas1.getText().toString());
+                        new RegisterRequestUtils(this, "register_data", "POST", "{\"email\": \"" + new_email.getText().toString() + "\", \"password\": \"" + pas1.getText().toString() + "\"}").execute(new_email.getText().toString(), pas1.getText().toString());
 
                         // Test line
-                        ToastUtils.showShortToast(getContext(), String.valueOf(status)+"вывод статуса для тыстирования, если True то сообщение на почту отправленно");
+                        ToastUtils.showShortToast(getContext(), (status)+"вывод статуса для тыстирования, если True то сообщение на почту отправленно");
 
                         if (status){
                             // Передача данных в FragmentRegisterCode
